0d4b2255861e800856d43f2ded09fca4cb59c743
mraliu <teachmrliu@gmail.com>
2024-12-19 07:20:36 +0000

    Take dmg

--- sw_game/game.py
+++ sw_game/game.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 from oop import *
 
 pygame.init()
@@ -9,20 +9,20 @@ font = pygame.font.SysFont('Arial', 20)
 
 bullets = []
     
-def displaytext(text):
+def displaytext(text, xpos, ypos): # Used for displaying text on screen
     text = font.render((str(text)), True, (255, 255, 255))
     textRect = text.get_rect()
-    textRect.center = (w*3/4), (50)
+    textRect.center = (xpos), (ypos)
     screen.blit(text, textRect)
 
 
 running = True
 clock = pygame.time.Clock() 
 
+# Instantiation objects
 falcon = Player(screen, "falcon.png", 70, 75)
 tie = Enemies(screen, "tiefighter.png", 50, 50, w/2, 50)
-
-ties = [Enemies(screen, "tiefighter.png", 50, 50, x*50+50, 50) for x in range(10)]
+ties = [Enemies(screen, "tiefighter.png", 50, 50, 25+x*75, random.randint(50, 150)) for x in range(10)]
 
 collide = False
 
@@ -40,6 +40,7 @@ while running:
     if ties:
         for tie in ties:
             tie.draw()
+            tie.moveto()
     
     if falcon.bullets:
         for b in falcon.bullets:
@@ -51,13 +52,15 @@ while running:
                 for tie in ties:
                     if b.collide(tie):
                         falcon.bullets.remove(b)
-                        ties.remove(tie)
+                        if tie.takedmg(b.dmg):
+                            ties.remove(tie)
                     elif b.ypos <=0:
                         falcon.bullets.remove(b)
             except:
                 pass
 
-    displaytext(collide)
+    displaytext(collide, w*3/4, 50)
+    displaytext(str(len(falcon.bullets)), w*3/4, 100)
 
     pygame.display.flip()
     clock.tick(60)
--- sw_game/oop.py
+++ sw_game/oop.py
@@ -8,16 +8,32 @@ class Player:
         self.surf = pygame.image.load(self.image).convert_alpha()
         self.surf = pygame.transform.scale(self.surf, (self.width, self.height))
         self.surf = pygame.transform.rotate(self.surf,(0))
+        self.dmgsurf = self.surf.copy()
+        self.dmgsurf.fill((255, 0, 0, 0), special_flags=pygame.BLEND_RGBA_MULT)
         self.xpos = 400
         self.ypos = 500
         self.rect = self.surf.get_rect(topleft=(self.xpos, self.ypos))
         self.xspeed = 10
         self.yspeed = 3 
+        self.hp = 100
         self.bullets = []
 
     def draw(self):
         self.rect = self.surf.get_rect(topleft=(self.xpos, self.ypos))
         self.screen.blit(self.surf, self.rect)
+
+    def takedmg(self, dmg):
+        if self.hp > 0:
+            self.hp -= dmg
+            self.surf = self.dmgsurf
+            # Not died
+            return False
+        else:
+            # Died
+            return True
+    
+        self.dmgsurf = pygame.copy()
+        self.dmgurf.fill((255, 0, 0, 0), special_flags=pygame.BLEND_RGBA_MULT)
         
     def move(self):
         global bullets
@@ -52,6 +68,7 @@ class Bullet:
         self.height = 10
         self.speed = 3
         self.rect = pygame.Rect(self.xpos, self.ypos, self.width, self.height)
+        self.dmg = 2
 
     def draw(self):
         self.rect = pygame.Rect(self.xpos, self.ypos, self.width, self.height)
@@ -66,4 +83,9 @@ class Enemies(Player):
     def __init__(self, screen, setImage, setWidth, setHeight, setX, setY):
         super().__init__(screen, setImage, setWidth, setHeight)
         self.xpos = setX
-        self.ypos = setY
+        self.ypos = 0 - (setY*2)
+        self.yend = setY
+
+    def moveto(self): # moves from outside of screen into starting position
+        if self.ypos <= self.yend:
+            self.ypos+=2
